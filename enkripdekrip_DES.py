e3e2cc6e90e4ece4b12fc6b21da85d487c137a89
ArTemis-jpg-themis <152158224+ArTemis-jpg-themis@users.noreply.github.com>
2023-11-28 18:27:51 +0700

    Update enkripdekrip_DES.py

--- enkripdekrip_DES.py
+++ enkripdekrip_DES.py
@@ -1,55 +1,26 @@
 import streamlit as st
+from Crypto.Cipher import DES
+from Crypto.Random import get_random_bytes
 
-# Fungsi DES manual (contoh sederhana, tidak aman untuk digunakan di produksi)
-def initial_permutation(block):
-    # Implement initial permutation
-    return block
+def pad_message(message):
+    # Pad the message with spaces to make its length a multiple of 8
+    while len(message) % 8 != 0:
+        message += ' '
+    return message
 
-def final_permutation(block):
-    # Implement final permutation
-    return block
+def des_encrypt(key, message):
+    cipher = DES.new(key, DES.MODE_ECB)
+    padded_message = pad_message(message)
+    encrypted_message = cipher.encrypt(padded_message.encode('utf-8'))
+    return encrypted_message
 
-def des_encrypt_block(block, key):
-    # Implement DES encryption for a single 64-bit block with a 56-bit key
-    # This function should include the initial permutation, 16 rounds of Feistel network, and the final permutation
-    return block
-
-def des_decrypt_block(block, key):
-    # Implement DES decryption for a single 64-bit block with a 56-bit key
-    # This function should include the initial permutation, 16 rounds of Feistel network, and the final permutation
-    return block
-
-def des_encrypt(plaintext, key):
-    # Implement DES encryption for the entire plaintext
-    # Break the plaintext into 64-bit blocks, encrypt each block, and concatenate the results
-    ciphertext = ""
-    for i in range(0, len(plaintext), 8):
-        block = initial_permutation(plaintext[i:i+8])
-        encrypted_block = des_encrypt_block(block, key)
-        ciphertext += final_permutation(encrypted_block)
-    return ciphertext
-
-def des_decrypt(ciphertext, key):
-    # Implement DES decryption for the entire ciphertext
-    # Break the ciphertext into 64-bit blocks, decrypt each block, and concatenate the results
-    plaintext = ""
-    for i in range(0, len(ciphertext), 8):
-        block = initial_permutation(ciphertext[i:i+8])
-        decrypted_block = des_decrypt_block(block, key)
-        plaintext += final_permutation(decrypted_block)
-    return plaintext
+def des_decrypt(key, encrypted_message):
+    cipher = DES.new(key, DES.MODE_ECB)
+    decrypted_message = cipher.decrypt(encrypted_message).decode('utf-8').rstrip()
+    return decrypted_message
 
 def main():
-    st.title("DES Encryption and Decryption with Streamlit")
-
-    st.markdown(
-        """
-        ## Data Encryption Standard (DES)
-        DES is a symmetric-key algorithm for the encryption of electronic data. It operates on 64-bit blocks of data using a 56-bit key.
-        This example provides a simple, manual implementation of DES for educational purposes.
-        Note: This implementation is not secure for production use.
-        """
-    )
+    st.title("DES Encryption and Decryption")
 
     key = st.text_input("Enter 8-character key:")
 
@@ -57,25 +28,24 @@ def main():
         st.warning("Please enter a valid 8-character key.")
         st.stop()
 
-    operation = st.selectbox("Select Operation", ["Encrypt", "Decrypt"])
+    message = st.text_area("Enter message:")
 
-    if operation == "Encrypt" or operation == "Decrypt":
-        message = st.text_area(f"Enter text to {operation.lower()}:")
+    encrypt_button = st.button("Encrypt")
+    decrypt_button = st.button("Decrypt")
 
-        if st.button(f"{operation}"):
-            key_bytes = key.encode('utf-8')
+    if encrypt_button:
+        key_bytes = key.encode('utf-8')
+        encrypted_message = des_encrypt(key_bytes, message)
+        st.success("Encryption successful:")
+        st.text("Encrypted Message:")
+        st.text(encrypted_message)
 
-            if operation == "Encrypt":
-                result = des_encrypt(message, key_bytes)
-                st.success(f"{operation}ion successful:")
-                st.text(f"{operation}ed Text:")
-                st.text(result)
-
-            elif operation == "Decrypt":
-                result = des_decrypt(message, key_bytes)
-                st.success(f"{operation}ion successful:")
-                st.text(f"{operation}ed Text:")
-                st.text(result)
+    if decrypt_button:
+        key_bytes = key.encode('utf-8')
+        decrypted_message = des_decrypt(key_bytes, message.encode('utf-8'))
+        st.success("Decryption successful:")
+        st.text("Decrypted Message:")
+        st.text(decrypted_message)
 
 if __name__ == "__main__":
     main()
