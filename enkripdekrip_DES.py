ba4195d6a0d6a041d0af680cc004694b781821bf
ArTemis-jpg-themis <152158224+ArTemis-jpg-themis@users.noreply.github.com>
2023-11-29 17:10:28 +0700

    enkripdekrip_DES.py

--- enkripdekrip_DES.py
+++ enkripdekrip_DES.py
@@ -1,6 +1,4 @@
 import streamlit as st
-# from Crypto.Cipher import DES
-# from Crypto.Random import get_random_bytes
 
 def pad_message(message):
     while len(message) % 8 != 0:
@@ -20,6 +18,7 @@ def des_decrypt_block(block, key):
     return block
 
 def des_encrypt(plaintext, key):
+    ciphertext = ""
     for i in range(0, len(plaintext), 8):
         block = initial_permutation(plaintext[i:i+8])
         encrypted_block = des_encrypt_block(block, key)
@@ -38,44 +37,44 @@ def main():
     st.title("DES Encryption and Decryption")
 
     st.markdown(
-            """
-            DES adalah salah satu dari contoh Algortima kriptografi modern.
-            \nDES Merupakan data dienkripsi dalam blok 64-bit menggunakan kunci internal 56-bit yang dibangkitkan dari kunci eksternal 64-bit.
-            """
-        )
+        """
+        DES adalah salah satu dari contoh Algoritma kriptografi modern.
+        \nDES Merupakan data dienkripsi dalam blok 64-bit menggunakan kunci internal 56-bit yang dibangkitkan dari kunci eksternal 64-bit.
+        """
+    )
 
     key = st.text_input("Masukkan kunci 8 karakter:")
 
     if len(key) != 8:
-        st.warning("Masukkan kunci dengan maksimal 8 karakter (contoh : 00000000)")
+        st.warning("Masukkan kunci dengan maksimal 8 karakter (contoh: 00000000)")
         st.stop()
 
-    pilihan = st.selectbox("Pilih opsi kriptografi : ", ["Enkripsi", "Dekripsi"])
+    pilihan = st.selectbox("Pilih opsi kriptografi:", ["Enkripsi", "Dekripsi"])
     st.warning(f"Opsi yang dipilih: {pilihan}")
 
-    if pilihan == "Enkripsi":
-        message = st.text_area(f"Masukkan kalimat untuk di{pilihan.lower()}:")
+    message = st.text_area(f"Masukkan kalimat untuk di{pilihan.lower()}:")
 
-        if st.button(f"{pilihan}"):
-            key_bytes = key.encode('utf-8')
+    if st.button(f"{pilihan}"):
+        key_bytes = key.encode('utf-8')
 
-            if pilihan == "Enkripsi":
-                result = des_encrypt(message, key_bytes)
-                st.success(f"{pilihan} berhasil.")
-                st.text(f"Pesan {pilihan} :")
-                st.text(result)
-    else:
-        if st.button(f"{pilihan}"):
-            key_bytes = key.encode('utf-8')
+        if pilihan == "Enkripsi":
+            result = des_encrypt(message, key_bytes)
+            st.success(f"{pilihan} berhasil.")
+            st.text(f"Pesan {pilihan} :")
+            st.text(result)
+        elif pilihan == "Dekripsi":
+            result = des_decrypt(message, key_bytes)
+            st.success(f"{pilihan} berhasil.")
+            st.text(f"Pesan {pilihan} :")
+            st.text(result)
 
-            if pilihan == "Dekripsi":
-                hasil = des_decrypt(message, key_bytes)
-                st.success(f"{pilihan} berhasil.")
-                st.text(f"Pesan {pilihan} :")
-                st.text(hasil)
-            
     st.write("""## Projek Kelompok 
-             
-             """)
+            1. Hanifah Az-zahra
+            2. Dinda Cantika Putri
+            3. Ika Kusuma W.
+            4. M. Alif Nadin Putra      22105110
+            5. Derajat Salim Wibowo     22105110??
+            """)
+
 if __name__ == "__main__":
     main()
