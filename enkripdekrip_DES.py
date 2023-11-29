be204f0ee74b0f5c39e07e744367052fb58ead1e
ArTemis-jpg-themis <152158224+ArTemis-jpg-themis@users.noreply.github.com>
2023-11-29 17:32:25 +0700

    Update enkripdekrip_DES.py

--- enkripdekrip_DES.py
+++ enkripdekrip_DES.py
@@ -1,23 +1,27 @@
 import streamlit as st
 
-def pad_message(message):
-    while len(message) % 8 != 0:
-        message += ' '
-    return message
-
+# Fungsi DES manual (contoh sederhana, tidak aman untuk digunakan di produksi)
 def initial_permutation(block):
+    # Implement initial permutation
     return block
 
 def final_permutation(block):
+    # Implement final permutation
     return block
 
 def des_encrypt_block(block, key):
+    # Implement DES encryption for a single 64-bit block with a 56-bit key
+    # This function should include the initial permutation, 16 rounds of Feistel network, and the final permutation
     return block
 
 def des_decrypt_block(block, key):
+    # Implement DES decryption for a single 64-bit block with a 56-bit key
+    # This function should include the initial permutation, 16 rounds of Feistel network, and the final permutation
     return block
 
 def des_encrypt(plaintext, key):
+    # Implement DES encryption for the entire plaintext
+    # Break the plaintext into 64-bit blocks, encrypt each block, and concatenate the results
     ciphertext = ""
     for i in range(0, len(plaintext), 8):
         block = initial_permutation(plaintext[i:i+8])
@@ -26,6 +30,8 @@ def des_encrypt(plaintext, key):
     return ciphertext
 
 def des_decrypt(ciphertext, key):
+    # Implement DES decryption for the entire ciphertext
+    # Break the ciphertext into 64-bit blocks, decrypt each block, and concatenate the results
     plaintext = ""
     for i in range(0, len(ciphertext), 8):
         block = initial_permutation(ciphertext[i:i+8])
@@ -34,47 +40,42 @@ def des_decrypt(ciphertext, key):
     return plaintext
 
 def main():
-    st.title("DES Encryption and Decryption")
+    st.title("DES Encryption and Decryption with Streamlit")
 
     st.markdown(
         """
-        DES adalah salah satu dari contoh Algoritma kriptografi modern.
-        \nDES Merupakan data dienkripsi dalam blok 64-bit menggunakan kunci internal 56-bit yang dibangkitkan dari kunci eksternal 64-bit.
+        ## Data Encryption Standard (DES)
+        DES is a symmetric-key algorithm for the encryption of electronic data. It operates on 64-bit blocks of data using a 56-bit key.
+        This example provides a simple, manual implementation of DES for educational purposes.
+        Note: This implementation is not secure for production use.
         """
     )
 
-    key = st.text_input("Masukkan kunci 8 karakter:")
+    key = st.text_input("Enter 8-character key:")
 
     if len(key) != 8:
-        st.warning("Masukkan kunci dengan maksimal 8 karakter (contoh: 00000000)")
+        st.warning("Please enter a valid 8-character key.")
         st.stop()
 
-    pilihan = st.selectbox("Pilih opsi kriptografi:", ["Enkripsi", "Dekripsi"])
-    st.warning(f"Opsi yang dipilih: {pilihan}")
+    operation = st.selectbox("Select Operation", ["Encrypt", "Decrypt"])
 
-    message = st.text_area(f"Masukkan kalimat untuk di{pilihan.lower()}:")
+    if operation == "Encrypt" or operation == "Decrypt":
+        message = st.text_area(f"Enter text to {operation.lower()}:")
 
-    if st.button(f"{pilihan}"):
-        key_bytes = key.encode('utf-8')
+        if st.button(f"{operation}"):
+            key_bytes = key.encode('utf-8')
 
-        if pilihan == "Enkripsi":
-            result = des_encrypt(message, key_bytes)
-            st.success(f"{pilihan} berhasil.")
-            st.text(f"Pesan {pilihan} :")
-            st.text(result)
-        elif pilihan == "Dekripsi":
-            result = des_decrypt(message, key_bytes)
-            st.success(f"{pilihan} berhasil.")
-            st.text(f"Pesan {pilihan} :")
-            st.text(result)
+            if operation == "Encrypt":
+                result = des_encrypt(message, key_bytes)
+                st.success(f"{operation}ion successful:")
+                st.text(f"{operation}ed Text:")
+                st.text(result)
 
-    st.write("""## Projek Kelompok 
-            1. Hanifah Az-zahra
-        2. Dinda Cantika Putri
-        3. Ika Kusuma W.
-        4. M. Alif Nadin Putra      22105110
-        5. Derajat Salim Wibowo     22105110??
-            """)
+            elif operation == "Decrypt":
+                result = des_decrypt(message, key_bytes)
+                st.success(f"{operation}ion successful:")
+                st.text(f"{operation}ed Text:")
+                st.text(result)
 
 if __name__ == "__main__":
     main()
